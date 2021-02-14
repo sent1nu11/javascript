dd89484acfd168e9432acb3c92e7e55b50dfc13d
sent1nu11 <sent1nu11@gmail.com>
2021-02-14 00:46:16 -0700

    modified for making cars

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-var taxi = {
-    make: "Webville Motors",
-    model: "Taxi",
-    year: 1955,
-    color: "yellow",
-    passengers: 4,
-    convertible: false,
-    mileage:    2810
+function makeCar() {
+    var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+    var models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
+    var years = [1955, 1957, 1948, 1954, 1961]
+    var colors = ["red", "blue", "tan", "yellow", "white"];
+    var convertible = [true, false];
+
+    var rand1 = Math.floor(Math.random() * makes.length);
+    var rand2 = Math.floor(Math.random() * models.length);
+    var rand3 = Math.floor(Math.random() * years.length);
+    var rand4 = Math.floor(Math.random() * colors.length);
+    var rand5 = Math.floor(Math.random() * 5) + 1;
+    var rand6 = Math.floor(Math.random() * 2);
+
+    
+
 }
 
-function prequal(car) {
-    if (car.mileage > 10000) {
-        return false;
-    } else if (car.year > 1960) {
-            return false;
-    }
-    return true;
-}
-
-var worthALook = prequal(taxi);
-
-if (worthALook) {
-    console.log("The " + taxi.model + " is worth a look!")
-} else {
-    console.log("Skip it.")
-}
