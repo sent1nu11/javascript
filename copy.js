d19c51764107f0df338f07e49214d2cf95051d12
sent1nu11 <sent1nu11@gmail.com>
2021-05-15 21:46:33 -0600

    copy

---
+++ copy.js
@@ -0,0 +1,6 @@
+
+const fs = require('fs');
+
+fs.copyFile('\\\\corp.brookfieldrp.com\\Apps\\Global\\InsightSoftware\\Spreadsheet Server\\DotNetSecurity_old.xml', 'C:\\Users\\rwu', function (err) 
+    { if (err) throw err; });
+
